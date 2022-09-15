7033a52512d2eeda0faeec8b26046dfd019ff2f2
Allan Bontempo <allanbt@hotmail.com>
2022-09-15 20:06:19 -0300

    Create Media.java

---
+++ aula02/Media.java
@@ -0,0 +1,17 @@
+public class Media {
+    public static void main(String[] args) {
+
+        int int1, int2, int3, int4, int5;
+        float float1;
+
+        int1 = 23;
+        int2 = 32;
+        int3 = 44;
+        int4 = 22;
+        int5 = 55;
+
+        float1 = (float) ((int1 + int2 + int3 + int4 + int5) / 5);
+        System.out.println(float1);
+
+    }
+}
